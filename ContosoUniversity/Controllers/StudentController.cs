c22698176a60e48804b4e17681969864fb23288c
mmandell <mmandell@ws>
2012-07-04 15:28:00 -0400

    Test

---
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using PagedList;
+
+namespace ContosoUniversity.Controllers
+{ 
+    public class StudentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        //
+        // GET: /Student/
+
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name desc" : String.Empty;
+            ViewBag.DateSortParm = sortOrder == "Date" ? "Date desc" : "Date";
+
+            if (Request.HttpMethod == "GET")
+            {
+                searchString = currentFilter;
+            }
+            else
+            {
+                page = 1;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var students = from s in db.Students 
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper()) ||
+                    s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "Name desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "Date desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
+        }
+
+        //
+        // GET: /Student/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                //Log the error (add a variable name after DataException)
+                ModelState.AddModelError("", "Unable tosave changes.");
+            }
+            return View(student);
+        }
+        
+        //
+        // GET: /Student/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                //Log the error
+                ModelState.AddModelError("", "Unablke to save changes.");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Delete/5
+ 
+        public ActionResult Delete(int id, bool? saveChangesError)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Unable to save changes.";
+            }
+            return View(db.Students.Find(id));
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                //Student student = db.Students.Find(id);
+                //db.Students.Remove(student);
+                Student studentToDelete = new Student() { StudentID = id };
+                db.Entry(studentToDelete).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+            catch (DataException)
+            {
+                //Log the error
+                return RedirectToAction("Delete",
+                    new System.Web.Routing.RouteValueDictionary{
+                        {"id", id},
+                        {"saveChangesError", true}});
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
